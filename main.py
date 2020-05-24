d7d56003e3ac1c63511ac81b866541415efd7427
Brian0615 <brianps.lam@mail.utoronto.ca>
2020-05-24 13:54:14 -0400

    add single simulation running

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ Last Updated    : May 03, 2020
 === UPDATE NOTES ===
  > May 24, 2020
     - move to root directory, update import statements
+    - add functionality to run one single simulation
  > May 08, 2020
     - file created
 """
@@ -25,9 +26,14 @@ parser = argparse.ArgumentParser(description='This script will run a series of \
     simulations with all possible combinations of anchor sets and random seeds given. \
     Note that both the arguments --anchor and --seed are REQUIRED.')
 
-parser.add_argument('-anchor', '-a', nargs=1, type=int, required=True, \
+parser.add_argument('sim_type', type=str)
+
+parser.add_argument('-dir', '-d', nargs=1, type=str, metavar='D')
+
+parser.add_argument('-anchor', '-a', type=int, default=0, \
     help='total number of anchor sets', metavar='A')
-parser.add_argument('-seed', '-s', nargs='+', type=int, required=True, \
+
+parser.add_argument('-seed', '-s', nargs='+', type=int, default=[], \
     help='the random seeds to be used', metavar='S')
 
 def write_config_file(anchor, seed):
@@ -46,27 +52,38 @@ def run_simulation():
 
 if __name__ == "__main__":
     # parse and extract arguments
-    args = parser.parse_args()
-    max_dilution = args.anchor[0]
-    seeds = args.seed
-    
-    for seed in seeds:
-        # find anchor sets for both upwards and downwards order
-        up_sets = list(range(1, max_dilution))
-        down_sets = list(range(max_dilution, 0, -1))
+    args = parser.parse_args() 
 
-        # upwards order
-        while len(up_sets) > 0: # for each dilution level
-            print(f"Testing with seed {seed} and anchor sets {up_sets}")
-            write_config_file(up_sets, seed)
-            run_simulation()
-            up_sets.pop(-1)
+    assert args.sim_type in ['series', 'single'], "ERROR: Must select sim_type as 'series' or 'single'"
+
+    if args.sim_type == "series":
+        max_dilution = args.anchor
+        seeds = args.seed
+
+        assert max_dilution > 0, "ERROR: Number of anchor sets must be greater than 0"
+        assert len(seeds) > 0, "ERROR: At least one random seed must be specified"
+
+        for seed in seeds:
+            # find anchor sets for both upwards and downwards order
+            up_sets = list(range(1, max_dilution))
+            down_sets = list(range(max_dilution, 0, -1))
+
+            # upwards order
+            while len(up_sets) > 0: # for each dilution level
+                print(f"Testing with seed {seed} and anchor sets {up_sets}")
+                write_config_file(up_sets, seed)
+                run_simulation()
+                up_sets.pop(-1)
+            
+            # downwards order
+            while len(down_sets) > 0: # for each dilution level 
+                print(f"Testing with seed {seed} and anchor sets {down_sets}")
+                write_config_file(down_sets, seed)
+                run_simulation()
+                down_sets.pop(-1)
+    
+    else:
+        run_simulation()
         
-        # downwards order
-        while len(down_sets) > 0: # for each dilution level 
-            print(f"Testing with seed {seed} and anchor sets {down_sets}")
-            write_config_file(down_sets, seed)
-            run_simulation()
-            down_sets.pop(-1)
         
 
